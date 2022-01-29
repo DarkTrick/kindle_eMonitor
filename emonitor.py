d644ad5d5a8130170800d4ed62c6194478287f02
DarkTrick <notebook22312@gmail.com>
2022-01-29 21:37:14 +0900

    bugfix: write() argument must be str, not bytes

--- emonitor.py
+++ emonitor.py
@@ -15,13 +15,13 @@ port=8080 #port of http server (http://127.0.0.1:8000/)
 #capture region
 #l,t=(1680,1050-800)
 l,t=(10,120) #left and right offset from primary monitor
-#w,h=(595,701) #width and height of capture region
-w,h=(701,595) #width and height of capture region
+#rootWindow,h=(595,701) #width and height of capture region
+rootWindow,h=(701,595) #width and height of capture region
 
 # "ss" = screenshot
 ssX = l
 ssY = t
-ssWidth = w
+ssWidth = rootWindow
 ssHeight = h
 
 #fn ='shot.jpg' #file name of screenshot
@@ -31,30 +31,54 @@ fn ='shot.png' #file name of screenshot
 
 import socket
 from http.server import BaseHTTPRequestHandler, HTTPServer
-
-
-import gtk.gdk
 import time
 
+
+# import GTK
+import gi
+gi.require_version("Gtk", "3.0")
+gi.require_version("Gdk", "3.0")
+from gi.repository import Gdk as gdk
+from gi.repository import GObject as gobject
+
+# Windows only?
+# import gtk.gdk as gdk
+
+class GdkPixbufAdapterLinux:
+  """
+  I'm not sure if the code here is same on win and linux;
+  thereore I created this adapter already - just in case.
+  """
+  def __init__(self, rootWindow, ssX,ssY,ssWidth,ssHeight):
+    self.buffer = gdk.pixbuf_get_from_window(rootWindow, ssX,ssY,ssWidth,ssHeight)
+
+  def save(self, filename, encoding = "png"):
+    if (not self.hasBuffer()):
+      return False
+
+    return self.buffer.savev (filename, encoding)
+
+  def hasBuffer(self):
+    return (self.buffer != None)
+
+  def rotate_simple(self, degrees):
+    self.buffer = self.buffer.rotate_simple(degrees)
+
+
+
+
 def gtkScreenshot():
-  w = gtk.gdk.get_default_root_window()
-  #ssWidth,ssHeight = w.get_size()
+  rootWindow = gdk.get_default_root_window()
+  #ssWidth,ssHeight = rootWindow.get_size()
   #print "The size of the window is %d x %d" % sz
 
-  pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,ssWidth,ssHeight)
-  pb = pb.get_from_drawable(w,w.get_colormap(),ssX,ssY,0, 0,ssWidth,ssHeight)
-
-  pb = pb.rotate_simple(90)
-
-  #pb = gtk.gdk.pixbuf_get_from_window(window, ssX, ssY, ssWidth, ssHeight)
+  image = GdkPixbufAdapterLinux (rootWindow, ssX,ssY,ssWidth,ssHeight)
+  image.rotate_simple(90)
 
   ts = time.time()
   filename = fn
 
-  if (pb != None):
-    pb.save(filename,"png")
-    #print "Screenshot saved to "+filename
-  else:
+  if (not image.save(filename,"png")):
     print("Unable to get the screenshot.")
 
 class serv(BaseHTTPRequestHandler):
@@ -84,10 +108,14 @@ class serv(BaseHTTPRequestHandler):
 
     </body>
     </html>"""
+
+
+
     with open("debug_website.html","w") as file:
       file.write(website)
 
-    self.wfile.write(website)
+    websiteBytes = bytes(website,"utf-8")
+    self.wfile.write(websiteBytes)
 
   def do_GET(self):
     self.send_response(200)
@@ -106,25 +134,29 @@ class serv(BaseHTTPRequestHandler):
     else:
       self.deliverSite()
 
-    try:
-      print( 'eMonitor by Kranu')
+def main():
+  try:
+    print( 'eMonitor by Kranu')
 
-      #Amazon's website is used here for its reliablity. Feel free to change it.
-      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-      s.connect(("amazon.com",80))
+    #Amazon's website is used here for its reliablity. Feel free to change it.
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("amazon.com",80))
 
-      print ('Starting.. '),
-      server=HTTPServer(('',port),serv)
-      print ('Press Ctrl+C to stop')
-      print ("")
+    print ('Starting.. '),
+    server=HTTPServer(('',port),serv)
+    print ('Press Ctrl+C to stop')
+    print ("")
 
-      print ('On your Kindle, visit http://'+s.getsockname()[0]+':'+str(port)+'/')
+    print ('On your Kindle, visit http://'+s.getsockname()[0]+':'+str(port)+'/')
 
-      # take initial screenshot
-      gtkScreenshot()
+    # take initial screenshot
+    gtkScreenshot()
 
 
-      server.serve_forever()
-    except KeyboardInterrupt:
-      print ('Stopping.. '),
-      server.socket.close()
+    server.serve_forever()
+  except KeyboardInterrupt:
+    print ('Stopping.. '),
+    server.socket.close()
+
+if (__name__ == "__main__"):
+  main()
